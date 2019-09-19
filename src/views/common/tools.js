40aa49f25022f5fb2c817e36fb711812a4b4ce78
Duans <2398208294@qq.com>
2019-09-20 01:11:19 +0800

    段海峰上传电影详情

---
+++ src/views/common/tools.js
@@ -0,0 +1,24 @@
+export default {
+    getWeek(len){ // 获取week
+        let date = [
+            ['今天', '明天', '后天'],
+            ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
+        ]
+        let index = new Date().getDay();
+        let week = [];
+        let bol = true;
+        for (let i = 0; i < len; i++) {
+            if (i >= 0 && i <= 2) {
+                week.push(date[0][i])
+            } else {
+                index = (index + (bol ? i : 0)) % 7;
+                bol = false;
+                if (index > week.length - 1) {
+                    index = 0;
+                }
+                week.push(date[1][index++]);
+            }
+        }
+        return week;
+    }
+}
