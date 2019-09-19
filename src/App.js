e9979b9e1db8bb6c04cea52a70473c7cb4e39a9f
Dylan-PL <Dylan188288@163.com>
2019-09-19 09:00:22 +0800

    解决路由问题，彭磊

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ class App extends React.Component{
     render(){
         return (
             <Router>
-
                 <Switch>
                 {
                     Routers.map((item,i)=>{
                     return   <Route key={item.path}  path={item.path} component={item.component}/>
                 })}
                     <Redirect  from={"/"} to={"/nav"}/>
-
                 </Switch>
 
             </Router>
